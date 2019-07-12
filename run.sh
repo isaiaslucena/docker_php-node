954cc07733f2e409a5080c24eaf90ee361f04e77
Isaias Neto <isaiasneto@gmail.com>
2019-07-12 12:05:57 -0300

    Correction run.sh

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@ if [[ "${FIRST_RUN}" == "**TRUE**" ]] ; then
 	npm install
 	/app/node_modules/bower/bin/bower --allow-root install
 	/app/node_modules/gulp/bin/gulp.js build
-	php artisan key:generate
 	unset FIRST_RUN
+	php artisan key:generate
 fi
 
 php artisan serve --port 80 --host 0.0.0.0
