89151fd3b132488a661e3bca34f65ca225ec8248
Isaias Neto <isaiasneto@gmail.com>
2020-04-06 18:11:11 -0300

    Final changes to run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,17 @@
 #!/bin/bash -x
 
 unset REDIS_PORT
+rm -rf /app/bootstrap/cache/*
 source .env
 
 if [[ -f "/FIRST_RUN" ]] ; then
 	rm -rf /FIRST_RUN
 	composer install
 	yarn install
-	rm -rf /app/bootstrap/cache/*
 	php artisan key:generate
 fi
 
 yarn run dev
+
+php artisan optimize:clear
 php artisan serve --port 8000 --host 0.0.0.0
